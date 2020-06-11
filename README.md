a2b7acf6ff04335a0441e8190a23320364f42204
saidatta1 <b118051@iiit-bh.ac.in>
2020-06-11 21:02:06 +0530

    frontend backend added

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
 ## Setting up the development environment
 
-1. Run npm install in home directory
+1. Run npm install inside `/server` directory
 2. Run npm install inside `/client` directory
 
 ## Running the project
 
-1. First start the server by the command `npm start` in home directory
+1. First start the server by the command `npm start` in `/server` directory
 2. Then start the client side server by moving to `/client` and by the command `npm start`
-3. Execute the above two steps in the specified order or you will get `proxy error`
-
-#### Testing Proxy:
-
-1. After the react server starts(step 2) open browser devtools and move to networks and check the response for `/proxytesting`
-the response should be `Proxy Success!`
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiresponse: "" }
+  }
+  callAPI() {
+    fetch("http://localhost:5000/test")
+      .then(res => res.text())
+      .then(res => this.setState({ apiresponse: res }))
+  }
+  componentWillMount() {
+    this.callAPI();
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Frontend</h1>
+        <p>{this.state.apiresponse}</p>
+      </div>
+    );
+  }
 }
-
 export default App;
