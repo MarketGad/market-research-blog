0838535075fcb249d3671e1fd6a239cb73d8ac77
saidatta1 <b118051@iiit-bh.ac.in>
2020-06-17 01:16:57 +0530

    icon added

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import EditorScreen from './screens/EditorScreen';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiresponse: "" }
-  }
-  callAPI() {
-    fetch("http://localhost:5000/")
-      .then(res => res.text())
-      .then(res => this.setState({ apiresponse: res }))
-  }
-  componentWillMount() {
-    this.callAPI();
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { apiresponse: "" }
+  // }
+  // callAPI() {
+  //   fetch("http://localhost:5000/")
+  //     .then(res => res.text())
+  //     .then(res => this.setState({ apiresponse: res }))
+  // }
+  // componentWillMount() {
+  //   this.callAPI();
+  // }
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <h1>Hello World from App.js</h1>
-          <Route path="/editor" component={ EditorScreen } />
-          <p>{this.state.apiresponse}</p>
+          <Route path="/editor" component={EditorScreen} />
+          {/* <p>{this.state.apiresponse}</p> */}
         </div>
       </BrowserRouter>
     );
