350931d1cff9c42a788483f43a85669f39dc49f9
Ajitesh13 <aji.yash13@gmail.com>
2020-07-12 23:44:02 +0530

    aws fix

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 })
 app.use("/api", googleloginroute);
