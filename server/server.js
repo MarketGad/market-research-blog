46c108ae23f37a5324ebc8fb6f74ebe365d1c166
Ajitesh13 <aji.yash13@gmail.com>
2020-07-12 21:26:10 +0530

    aws server fix

--- server/server.js
+++ server/server.js
@@ -55,6 +55,9 @@ app.use(function (err, req, res, next) {
 });
 
 const port = 5000;
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../../client/build/index.html`));
+});
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
