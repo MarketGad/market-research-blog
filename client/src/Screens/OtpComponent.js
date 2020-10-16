8b44c9e933339b7287acb1dbffaf5d61ad02aee7
Saidatta Sahu <saidatta.sahu02@gmail.com>
2020-10-17 00:53:14 +0530

    otp fix

--- client/src/Screens/OtpComponent.js
+++ client/src/Screens/OtpComponent.js
@@ -1,23 +1,23 @@
-import React, { Component } from "react";
-import OtpInput from "react-otp-input";
+import React, { Component } from 'react';
+import OtpInput from 'react-otp-input';
 
 export default class Otp extends Component {
-  state = { otp: "" };
+	state = { otp: '' };
 
-  handleChange = (otp) => this.setState({ otp });
+	handleChange = (otp) => this.setState({ otp });
 
-  render() {
-    return (
-      <OtpInput
-        value={this.state.otp}
-        onChange={this.handleChange}
-        numInputs={6}
-        separator={<span>-</span>}
-        inputStyle={{
-          border: "3px solid gray",
-          borderRadius: "7px",
-        }}
-      />
-    );
-  }
+	render () {
+		return (
+			<OtpInput
+				inputStyle={{
+					border: '2px solid gray',
+					borderRadius: '5px'
+				}}
+				value={this.state.otp}
+				onChange={this.handleChange}
+				numInputs={6}
+				separator={<span>-</span>}
+			/>
+		);
+	}
 }
