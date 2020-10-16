389764adb435e167c49c2440948c271825909959
Nutella Enthusiast <ritika.iyer2000@gmail.com>
2020-10-16 22:10:16 +0530

    pop up updates

---
+++ client/src/Screens/OtpComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import OtpInput from 'react-otp-input';
+
+export default class Otp extends Component {
+  state = { otp: '' };
+
+  handleChange = otp => this.setState({ otp });
+
+  render() {
+    return (
+      <OtpInput
+        value={this.state.otp}
+        onChange={this.handleChange}
+        numInputs={6}
+        separator={<span>-</span>}
+        
+      />
+    );
+  }
+}
